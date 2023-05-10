4fc9be3b79d53195ddab4574b4f2cb2c9f64db37
sandu-o-O <133158652+sandu-o-O@users.noreply.github.com>
2023-05-10 14:09:29 -0500

    sample test

---
+++ m7-p1.py
@@ -0,0 +1,9 @@
+'''
+Module 7: User Input and While Loops
+Program 1: The input ( ) function
+'''
+
+# input function pauses our program and waits for user entry
+message = input ("Please enter your last name: ")    # last name is assigned to variable message
+
+print ("\n Hello, Mr. " + message.upper() + " !")    # and then display back to user
